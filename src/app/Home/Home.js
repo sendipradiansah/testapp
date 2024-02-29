6a078510fe9761fe7af1d2a92b36d4269f17929a
USER <user@Diars-MacBook-Pro.local>
2024-03-01 00:36:44 +0700

    first upload project

--- src/app/Home/Home.js
+++ src/app/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { UseDispatch, useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getMahasiswa, mahasiswaSelector } from "../../features/mahasiswaSlice";
 import './Home.css';
 
--- src/components/ShowMahasiswa.js
+++ src/components/ShowMahasiswa.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getMahasiswa, mahasiswaSelector, deleteMahasiswa } from '../features/mahasiswaSlice';
 import { Link } from 'react-router-dom';
@@ -8,14 +8,35 @@ const ShowMahasiswa = () => {
   const dispatch = useDispatch();
   const mahasiswa = useSelector(mahasiswaSelector.selectAll); 
 
+  //search
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchInput, setSearchInput] = useState('');
+
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if(searchInput !== ''){
+      const filteredData = mahasiswa.filter(item => {
+        return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase());
+      });
+      setSearchResults(filteredData);
+    }
+
+  }
+  ////////
+
   useEffect(() => {
     dispatch(getMahasiswa());
   }, [dispatch])
 
   return (
     <div>
-      <Link to="/add" className="btn btn-success mb-4">Tambah</Link>
-      <table class="table table-bordered">
+      <div className="d-flex justify-content-between">
+        <Link to="/add" className="btn btn-success mb-4">Tambah</Link>
+        <div className="col-md-3 col-md-3">
+          <input type="text" className="form-control" id="search" placeholder="Search..." onChange={(e) => searchItems(e.target.value)}></input>
+        </div>
+      </div>
+      <table className="table table-bordered">
         <thead>
           <tr>
             <th className="text-center">No.</th>
@@ -27,21 +48,44 @@ const ShowMahasiswa = () => {
           </tr>
         </thead>
         <tbody>
-          {mahasiswa.map((item, index) => {
-            return(
-              <tr key={index}>
-                <td className="text-center">{index+1}.</td>
-                <td>{item.nim}</td>
-                <td>{item.nama}</td>
-                <td>{item.alamat}</td>
-                <td>{item.jurusan}</td>
-                <td className="text-center p-3">
-                  <Link to={`/edit/${item.id}`} className="btn btn-primary">Ubah</Link>&nbsp;&nbsp;&nbsp;
-                  <button onClick={() => {if(window.confirm(`Apakah anda yakin menghapus NIM ${item.nim}?`)) dispatch(deleteMahasiswa(item.id))}}className="btn btn-danger">Hapus</button>
-                </td>
-              </tr>
-            )
-          })}
+          {
+          searchInput.length > 1 ? 
+          (
+            searchResults.map((item, index) => {
+              return(
+                <tr key={index}>
+                  <td className="text-center">{index+1}.</td>
+                  <td>{item.nim}</td>
+                  <td>{item.nama}</td>
+                  <td>{item.alamat}</td>
+                  <td>{item.jurusan}</td>
+                  <td className="text-center p-3">
+                    <Link to={`/edit/${item.id}`} className="btn btn-primary">Ubah</Link>&nbsp;&nbsp;&nbsp;
+                    <button onClick={() => {if(window.confirm(`Apakah anda yakin menghapus NIM ${item.nim}?`)) dispatch(deleteMahasiswa(item.id))}}className="btn btn-danger">Hapus</button>
+                  </td>
+                </tr>
+              )
+            })
+          )
+          :
+          (
+            mahasiswa.map((item, index) => {
+              return(
+                <tr key={index}>
+                  <td className="text-center">{index+1}.</td>
+                  <td>{item.nim}</td>
+                  <td>{item.nama}</td>
+                  <td>{item.alamat}</td>
+                  <td>{item.jurusan}</td>
+                  <td className="text-center p-3">
+                    <Link to={`/edit/${item.id}`} className="btn btn-primary">Ubah</Link>&nbsp;&nbsp;&nbsp;
+                    <button onClick={() => {if(window.confirm(`Apakah anda yakin menghapus NIM ${item.nim}?`)) dispatch(deleteMahasiswa(item.id))}}className="btn btn-danger">Hapus</button>
+                  </td>
+                </tr>
+              )
+            })
+          )
+        }
         </tbody>
       </table>
     </div>
